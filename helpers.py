6f90a3fff7f62a618b756d713dc76ab4a2988452
late-bloomer82 <nateriver72@outlook.com>
2026-06-08 17:05:12 -0400

    refactor bruteforce lenght validation in separate helper file

--- helpers.py
+++ helpers.py
@@ -10,6 +10,20 @@ def hash_password(plain_password, selected_hash_algorithm):
     hash_obj.update(plain_password.encode())
     return hash_obj.hexdigest()
 
+def validate_bruteforce_lengths(min_length, max_length):
+    if min_length < 1:
+        print("Minimum password length must be at least 1.")
+        return False
+
+    if max_length > 7:
+        print("Maximum password length must be 7 or lower.")
+        return False
+
+    if min_length > max_length:
+        print("Minimum length must be less than or equal to maximum length.")
+        return False
+
+    return True
 
 def normalize_user_password_hash(user_password_hash):
     return user_password_hash.lower().strip()
